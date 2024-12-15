72c9ead03b469e2e377c1d18021a42dc423a4dbe
TCHANDA-ervan <94933948+TCHANDA-ervan@users.noreply.github.com>
2024-12-15 03:11:30 +0100

    navbar

--- portfolio/components/ui/Nav.jsx
+++ portfolio/components/ui/Nav.jsx
@@ -1,10 +1,52 @@
+"use client";
+
+
+import Link from "next/link";
+import { usePathname} from "next/navigation";
+
+const links = [
+    {
+        name : "home",
+        path: "/",
+    },
+    {
+        name : "Services",
+        path: "/Services",
+    },
+    {
+        name : "resume",
+        path: "/resume",
+    },
+    {
+        name : "work",
+        path: "/work",
+    },
+    {
+        name : "contact",
+        path: "/contact",
+    },
+   
+];
 const Nav = () => {
+    const pathname = usePathname();  // Correctement obtenir le pathname
+  
     return (
-      <nav>
-        desktop nav
+      <nav className="flex gap-8">
+        {links.map((link, index) => (
+          <Link
+            href={link.path}
+            key={index}
+            className={`${
+              link.path === pathname
+                && "text-accent-DEFAULT border-b-2 border-accent-DEFAULT "
+                
+            } capitalize font-medium hover:text-accent-hover transition-all`}
+          >
+            {link.name}
+          </Link>
+        ))}
       </nav>
     );
   };
   
   export default Nav;
-  
