b9ca82d5c754999570c0a0b0bb4f656f33d417b6
TCHANDA <je.tchanda-shanda@sdh.fr>
2025-01-06 16:57:02 +0100

    first day

--- portfolio/app/Resume/page.jsx
+++ portfolio/app/Resume/page.jsx
@@ -106,6 +106,7 @@ const education = {
       institution: "3il ingenieur",
       degree : "Bachelor",
       duration: "2022 - present",
+      Formation: "Mangement des solution digitales et data",
     },
     {
       institution: "3il ingenieur",
@@ -213,7 +214,29 @@ return (
 
 {/*education*/}
 <TabsContent value="education" className="w-full">
-  education
+<div className="flex flex-col gap-[30px] text-center xl:text-left">
+<h3 className="text-4xl font-bold">{education.title}</h3>
+<p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+{education.description}
+</p>
+<ScrollArea className="h-[400px]">
+<ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+{education.items.map((item, index) => {
+return (
+<li key={index} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1">
+<span className="text-green-500">{item.duration}</span>
+<h3 className="text-lg lg:text-xl font-medium max-w-[260px] min-h-[60px] text-center lg:text-left text-gray-400 leading-relaxed">{item.degree}</h3>
+<div className="flex items-center gap-3">
+{/*dot*/}
+<span className="w-[6px] h-[6px] rounded-full bg-green-500"></span>
+<p className="text-white/60">{item.institution}</p>
+</div>
+</li>
+);
+})}
+</ul>
+</ScrollArea>
+</div>
 </TabsContent>
 
 
